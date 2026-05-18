590bdace493cbf7a9a21bbc7dd17b7b2685d5dda
Collins Ikiara <120456288+CollinsIkiara@users.noreply.github.com>
2026-05-18 16:41:44 +0300

    Added a title to the Tech section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,16 +3,23 @@ import React from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { styles } from "../styles";
 
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
-    </div>
+    <>
+      <div className="text-center mb-10">
+        <p className={styles.sectionSubText}>What I work with</p>
+        <h2 className={styles.sectionHeadText}>My Tech Stack.</h2>
+      </div>
+      <div className='flex flex-row flex-wrap justify-center gap-10'>
+        {technologies.map((technology) => (
+          <div className='w-28 h-28' key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
